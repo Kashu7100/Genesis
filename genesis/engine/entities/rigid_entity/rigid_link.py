812a0548a74a41363c7f4d6c9431cefd867b8eae
Kashu7100 <echo.six0566@gmail.com>
2026-02-24 14:06:51 -0500

    clean up

--- genesis/engine/entities/rigid_entity/rigid_link.py
+++ genesis/engine/entities/rigid_entity/rigid_link.py
@@ -31,9 +31,6 @@ class RigidLink(RBC):
     multiple RigidGeoms (`link.geoms`, for collision) and RigidVisGeoms (`link.vgeoms` for visualization).
     """
 
-    # Subclasses can override to use a custom Geom class
-    _GeomClass = RigidGeom
-
     def __init__(
         self,
         entity: "RigidEntity",
@@ -316,7 +313,7 @@ class RigidLink(RBC):
         conaffinity=1,
         data=None,
     ):
-        geom = self._GeomClass(
+        geom = RigidGeom(
             link=self,
             idx=self.n_geoms + self._geom_start,
             cell_start=self.n_cells + self._cell_start,
